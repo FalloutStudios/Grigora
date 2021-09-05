611d93dced058f6b927cfc6eac68eff1be653bc8
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-09-05 19:29:11 +0800

    Player kinda working now still no output file

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const fs = require('fs');
 // Config
 let config = parseConfig();
 
+// Player Connection
+
+let currentConnected = 0;
+newBot(currentConnected, config['server']['ip'], config['server']['port'], config['server']['version']);
+
 // Green: \x1b[32m%s\x1b[0m
 // Orange: \x1b[33m%s\x1b[0m
 // Red: \x1b[31m%s\x1b[0m
@@ -29,6 +34,12 @@ function parseConfig (path = 'config/config.yml') {
         }
         if(getContents.server.port == null){
             getContents.server.port = prompt('Server Port: ');
+        }
+
+        // Invalid port returns error
+        if (isNaN(getContents.server.port) || typeof getContents.server.port != 'undefined' && isNaN(getContents.server.port) || getContents.server.port > 65535 || getContents.server.port < 1) { 
+            console.error('\x1b[31m%s\x1b[0m', '[Error - Mincraft Bot] Invalid Port: '+getContents.server.port); 
+            process.exit(0);        
         }
 
         // Get inline Playername
@@ -63,4 +74,63 @@ function parseConfig (path = 'config/config.yml') {
     console.log();
 
     return parsedYML;
+}
+
+//Create bot
+function newBot(currentPlayer = 0, ip = 'localhost', port = 25565, version = null){
+
+    // Parse ip and port
+    ip = ip.trim().toLowerCase();
+    port = parseInt(port, 10);
+
+    console.log(currentPlayer + ' - ' +config['players'][currentPlayer]);
+
+    let name = config['players'][currentPlayer];
+        name = name.trim();
+
+    console.log('[Log - Mincraft Bot] Starting '+name+' v'+version);
+
+    // Set to default server port if null
+    if(port == null || isNaN(port)){
+        port = 25565;
+    }
+
+    // Invalid name returns error
+    if(name == '' || name == null || typeof name != 'string' || name.length > 16 || name.length <= 4){
+        console.error('\x1b[31m%s\x1b[0m','[Error - Minecraft Bot] '+name+' is Invalid name');
+        return;
+    }
+
+    // Prevent object length overflow
+    if(currentPlayer > config['players'].length) return;
+
+    var bot = mineflayer.createBot({
+        host: ip,
+        port: port,
+        username: name,
+        version: version
+    });
+
+    console.log('[Log - Mincraft Bot] Connecting '+name+' v'+version);
+
+    bot.on('spawn', function(){
+        console.log();
+        console.log('Bot '+name+' coords: '+bot.entity.position);
+        console.log();
+
+        bot.quit();
+        bot.end();
+    });
+
+    bot.on('end', function(){
+        console.log('[Log - Minecraft Bot] Ended: '+name);
+
+        // New Player
+        currentConnected++;
+        newBot(currentConnected, config['server']['ip'], config['server']['port'], config['server']['version']);
+    });
+
+    bot.on('error', function (reason){
+        console.error('\x1b[31m%s\x1b[0m','[Error - Minecraft Bot] '+reason);
+    });
 }
