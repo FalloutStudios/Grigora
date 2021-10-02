4f039a0038b9ecc8acef9bfd8a7d02b796bb13f0
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-10-02 10:33:04 +0800

    Update startup.js

--- scripts/startup.js
+++ scripts/startup.js
@@ -1,4 +1,5 @@
 const Logger = require('./logger');
+const Version = require('./version');
 let log = new Logger();
     log.defaultPrefix = 'Startup';
 module.exports = () => {
@@ -11,4 +12,6 @@ module.exports = () => {
     log.log(`  ░   ░   ░▒ ░ ▒░ ▒ ░  ░   ░   ░ ▒ ▒░   ░▒ ░ ▒░  ▒   ▒▒ ░`);
     log.log(`░ ░   ░   ░░   ░  ▒ ░░ ░   ░ ░ ░ ░ ▒    ░░   ░   ░   ▒   `);
     log.log(`      ░    ░      ░        ░     ░ ░     ░           ░  ░`);
+    log.log('Grigora Version - ' + Version);
+    log.log('GitHub: https://github.com/FalloutStudios/Grigora');
 }
