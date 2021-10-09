ad141507cc62fc9a49cde0101f776da3fea5504e
GhexterCortes <69035887+GhexterCortes@users.noreply.github.com>
2021-10-09 16:51:34 +0800

    outputangina talaga

--- scripts/output.js
+++ scripts/output.js
@@ -13,23 +13,34 @@ module.exports = function () {
     this.output = { servers: {} };
 
     this.add = (descriptions = {}) => {
-
+        return descriptions;
     }
     this.write = () => {
         if(!this.location || this.location == null) { 
             log.error('No output location specified');
             return;
         }
+        if(!this.records || this.records == null) {
+            log.error('No records specified');
+            return;
+        }
 
         let contents = this.output;
         if(!Fs.existsSync(this.location)) {
+            log.warn('Creating records file');
             Fs.writeFileSync(this.location, Yml.stringify(this.output));
         } else {
+            log.warn('Reading records file');
             contents = Yml.parse(Fs.readFileSync(this.location, 'utf-8'));
         }
 
         if(!contents.servers[this.outputId] || typeof contents.servers[this.outputId] == 'undefined') contents.servers[this.outputId] = {};
-
-        contents.servers[this.outputId].concat(records);
+        log.log(contents);
+        
+        log.warn('Editing records');
+        for (const value of Object.keys(this.records)) {
+            contents.servers[this.outputId][value] = this.records[value];
+        }
+        log.log(contents);
     }
 }
